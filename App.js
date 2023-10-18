a9ee2af44657d1d6a33bf2777bc8e02031013d80
Luke Millam <lukemillam@Lukes-MacBook-Pro.local>
2023-10-17 19:39:44 -0700

    connected weather api, and set state variables

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {API_KEY} from './utils/weatherAPIKey';
+import Weather from './components/weather'
 
+let state = {
+  isLoading: true,
+  temperature: 0,
+  weatherCondition: null,
+  error: null
+};
 
-const isLoading = true
+function componentDidMount() {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      this.fetchWeather(position.coords.latitude, position.coords.longitude);
+    },
+    error => {
+      this.setState({
+        error: 'Error Gettig Weather Condtions'
+      });
+    }
+  );
+}
+
+function fetchWeather(lat = 25, lon = 25) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+  )
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+    });
+}
 
 export default function App() {
+  let {isLoading} = state
   return (
     <View style={styles.container}>
       {isLoading ? (
-        <View>
-          <Text>Weather App</Text>
-        </View>
+        <Text>Fetching</Text>
       ) : (
-        <View>
-          <Text>Fetching</Text>
-        </View>
+        <Weather/>
       )}
       <StatusBar style="auto" />
     </View>
---
+++ utils/weatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '94e7265149aaaf2e3bb42ce2a1d13ac3'
